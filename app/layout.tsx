86d925d6f76847b1596f18ccc5630e4c436c6562
PavitarSharma <pavitarsharma144@gmail.com>
2023-04-07 21:09:17 +0530

    reservation

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModal from "./components/modals/RentModal";
+import SearchModal from "./components/modals/SearchModal";
 
 export const metadata = {
   title: "Airbnb",
@@ -31,11 +32,10 @@ export default async function RootLayout({
           <RentModal />
           <LoginModal />
           <RegisterModal />
+          <SearchModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        <div className="pb-20 pt-28">
-          {children}
-        </div>
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
   );
